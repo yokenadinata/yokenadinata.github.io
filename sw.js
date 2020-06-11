581952683822cee0a10d66138c8d7175043b7ef6
Eric Tirtana <erictirtana13@gmail.com>
2020-06-11 15:48:33 +0700

    coba

--- sw.js
+++ sw.js
@@ -1,38 +1,41 @@
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('first-app')
-        .then(function(cache) {
-          cache.addAll([
-            '/',
-            '/index.html',
-            '/script/index.js'
-          ])
-        })
-    );
-    return self.clients.claim();
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(res) {
-          return res;
-        })
-    );
-  });
+  event.waitUntil(
+    caches.open('first-app')
+      .then(function(cache) {
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/script/index.js',
+          '/resource/bootstrap.min.css',
+          '/resource/bootstrap.min.js',
+          '/resource/jquery.min.css'
+        ])
+      })
+  );
+  return self.clients.claim();
+});
 
-  self.addEventListener('activate', function(event) {
-    console.log('[Service Worker] Activating Service Worker ....', event);
-    event.waitUntil(
-      caches.keys()
-        .then(function(keyList) {
-          return Promise.all(keyList.map(function(key) {
-            if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-              console.log('[Service Worker] Removing old cache.', key);
-              return caches.delete(key);
-            }
-          }));
-        })
-    );
-    return self.clients.claim();
-  });
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(res) {
+        return res;
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
+  return self.clients.claim();
+});
