c3a110fabfe44610bf2cbb396098ace77f9b33dd
Eric Tirtana <erictirtana13@gmail.com>
2020-06-11 16:55:37 +0700

    coba swjs

--- sw.js
+++ sw.js
@@ -1,7 +1,35 @@
+// self.addEventListener('install', function(event) {
+//   event.waitUntil(
+//     caches.open('first-app')
+//       .then(function(cache) {
+//         cache.addAll([
+//           '/',
+//           '/index.html',
+//           '/script/index.js',
+//           '/resource/bootstrap.min.css',
+//           '/resource/bootstrap.min.js',
+//           '/resource/jquery.min.css'
+//         ])
+//       })
+//   );
+//   return self.clients.claim();
+// });
+
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(res) {
+//         return res;
+//       })
+//   );
+// });
+
 self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
-    caches.open('first-app')
+    caches.open('static')
       .then(function(cache) {
+        console.log('[Service Worker] Precaching App Shell');
         cache.addAll([
           '/',
           '/index.html',
@@ -11,15 +39,14 @@ self.addEventListener('install', function(event) {
           '/resource/jquery.min.css'
         ])
       })
-  );
+  )
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
   return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(res) {
-        return res;
-      })
-  );
+  event.respondWith(fetch(event.request));
 });
