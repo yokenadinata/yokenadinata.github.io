7418aa77a8e84bc242e485c606dffe95d05db738
Eric Tirtana <erictirtana13@gmail.com>
2020-06-11 16:00:19 +0700

    hapus active

--- sw.js
+++ sw.js
@@ -23,19 +23,3 @@ self.addEventListener('fetch', function(event) {
       })
   );
 });
-
-self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ....', event);
-  event.waitUntil(
-    caches.keys()
-      .then(function(keyList) {
-        return Promise.all(keyList.map(function(key) {
-          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-            console.log('[Service Worker] Removing old cache.', key);
-            return caches.delete(key);
-          }
-        }));
-      })
-  );
-  return self.clients.claim();
-});
