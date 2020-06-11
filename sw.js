2acd4e5b9e093779f65741c46bac547b53b85455
Eric Tirtana <erictirtana13@gmail.com>
2020-06-11 17:08:53 +0700

    swjs

--- sw.js
+++ sw.js
@@ -42,10 +42,5 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(res) {
-        return res;
-      })
-  );
+  event.respondWith(fetch(event.request));
 });
