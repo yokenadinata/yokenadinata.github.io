5994ddf600ff728df0eabd91059ad507da4af4b7
etirtana <45303233+etirtana@users.noreply.github.com>
2020-06-11 15:33:18 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -20,3 +20,19 @@ self.addEventListener('install', function(event) {
         })
     );
   });
+
+  self.addEventListener('activate', function(event) {
+    console.log('[Service Worker] Activating Service Worker ....', event);
+    event.waitUntil(
+      caches.keys()
+        .then(function(keyList) {
+          return Promise.all(keyList.map(function(key) {
+            if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+              console.log('[Service Worker] Removing old cache.', key);
+              return caches.delete(key);
+            }
+          }));
+        })
+    );
+    return self.clients.claim();
+  });
